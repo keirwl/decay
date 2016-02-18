4b188aa7848feb583306c34f25b360926009cbde
Keir Little <me@keirlittle.com>
2016-02-18 15:16:55 +0000

    adds bin_decay skeleton, adds .DS_Store to ignores

---
+++ bin_decay.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void die(const char *message)
+{
+    if (errno) {
+        perror(message);
+    } else {
+        printf("ERROR: %s\n", message);
+    }
+
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
