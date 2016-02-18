6c3e1362f1e3937b62ab8308f5cdffcf05c3a725
Keir Little <me@keirlittle.com>
2016-02-18 16:30:25 +0000

    bin_decay is functional

--- bin_decay.c
+++ bin_decay.c
@@ -15,5 +15,42 @@ void die(const char *message)
 
 int main(int argc, char *argv[])
 {
+    if (argc != 4) die("Three arguments are required:\nbin_decay.app number_bins bin_width number_values");
+
+    const int number_bins = atoi(argv[1]);
+    const double bin_width = atof(argv[2]);
+    const int number_values = atoi(argv[3]);
+
+    if (number_bins < 1 || bin_width <= 0) die("Bin number and width must be positive values.");
+
+    double data[number_values];
+    char line[16];
+    int i = 0;
+    while (fgets(line, sizeof(line), stdin)) {
+        data[i] = atof(line);
+        i++;
+    }
+
+    double t[number_bins];
+    for (i = 0; i < number_bins; i++) {
+        t[i] = (i+1)*bin_width;
+    }
+
+    int n[number_bins];
+    // n is the histogram counter, thus must be initialised to zeroes
+    // this must be done by looping as it is dynamically allocated
+    for (i = 0; i < number_bins; i++) {
+        n[i] = 0;
+    }
+
+    int bin_index;
+    for (i = 0; i < number_values; i++) {
+        bin_index = (int)(data[i]/bin_width);
+        n[bin_index] += 1;
+    }
+
+    for (i = 0; i < number_bins; i++) {
+        printf("%g %i\n", t[i], n[i]);
+    }
     return 0;
 }
